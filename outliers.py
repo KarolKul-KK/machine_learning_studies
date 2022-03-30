d606eb41654f8b15cc32355e692f188f834f1cad
KarolKul-KK <>
2022-03-30 18:47:33 +0200

    Added quantile function

--- outliers.py
+++ outliers.py
@@ -26,3 +26,20 @@ def m_zscore_outliers(df: pd.DataFrame) -> Tuple[pd.DataFrame, int]:
         df_new = df.where(df_zscore[f'{column}'] < 3.5).dropna().reset_index(drop=True)
 
     return df_new, (len(df) - len(df_new))
+
+
+def quantile_outliers(df: pd.DataFrame) -> Tuple[pd.DataFrame, int]:
+
+    df_new = df.copy()
+    q_min = df.quantile(q=0.1)
+    q_max =df.quantile(q=0.9)
+
+    for i in range(len(df)):
+        if (df.iloc[i] > q_max).any():
+            df_new.drop(i, inplace=True)
+        elif (df.iloc[i] < q_min).any():
+            df_new.drop(i, inplace=True)
+        else:
+            pass
+        
+    return df_new.reset_index().drop('index', axis=1), (len(df) - len(df_new))
